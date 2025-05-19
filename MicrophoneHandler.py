4182c2dddcd301737f0d13fb0a2bde8339f1e726
Barnold88 <bandslover@outlook.com>
2025-05-20 00:48:34 +0100

    Rejig settings.json format and add error checking and fix bugs

--- MicrophoneHandler.py
+++ MicrophoneHandler.py
@@ -1,21 +1,25 @@
 import speech_recognition as sr
+import json
 
-class MicHandler:
+class MicrophoneHandler:
 
     def __init__(self,saveLocation=None,calibrationDuration=None):
 
         self.recognizer = sr.Recognizer()
-        
+        self.micName = None 
+        self.micIndex = None
         loaded_microphone = self.LoadMicrophone()
 
         if loaded_microphone != -1:
+            print(loaded_microphone)
             self.micIndex,self.micName = loaded_microphone
         else:
-            self.micIndex,self.micName = MicHandler.GetMicrophoneInformation()
+            self.micIndex,self.micName = MicrophoneHandler.GetMicrophoneInformation()
 
         self.microphone = sr.Microphone(device_index=self.micIndex)
         
-        self.recognizer.adjust_for_ambient_noise(self.microphone) 
+        if calibrationDuration:
+            self.CalibrateNoiseAdjustment(calibrationDuration)
 
     def GetMicrophoneInformation():
 
@@ -34,7 +38,7 @@ class MicHandler:
         
         return [mic_index,microphones[mic_index]]
 
-    def GetPhrase(self):
+    def GetPhrase(self,saveLocation=None):
 
         with self.microphone as source:
 
@@ -49,9 +53,9 @@ class MicHandler:
 
     def SaveMicrophone(self):
         with open("Data/MicrophoneSave.micSave","w") as saveFile:
-            saveFile.write(f"Mic: {self.m_micName}")
+            saveFile.write(f"Mic: {self.micName}")
 
-    def LoadMicrophone(self):
+    def LoadMicrophone(self,loadLocation=None):
 
         try:
             with open("Data/MicrophoneSave.micSave","r") as file:
@@ -72,10 +76,36 @@ class MicHandler:
                             return [microphones.index(possible[0]),possible[0]]
                         else:
                             return -1
+                else:
+                    return -1
                 
         except FileNotFoundError as fileNotFoundErr:
             return -1
         
     def CalibrateNoiseAdjustment(self,duration):
-        print(f"Calibrating for ambient noise. Please wait {duration} seconds...")
-        self.recognizer.adjust_for_ambient_noise(self.microphone,duration)
+        grammar = "seconds" if duration > 1 else "second"
+        print(f"Calibrating for ambient noise. Please wait {duration} {grammar}...")
+        with self.microphone as source:
+            self.recognizer.adjust_for_ambient_noise(self.microphone,duration)
+
+    def initMicrophone():
+        try:
+            with open("Data/settings.json","r") as file:
+                try:
+                    settings = json.load(file)
+                    useDuration = 1 if settings["Calibration"]["use"] == True else 0
+                    useSaveLocation = settings["SaveFile"]["location"] if settings["SaveFile"]["use"] == True else None
+
+                    return MicrophoneHandler(
+                        saveLocation        = useSaveLocation,
+                        calibrationDuration = settings["Calibration"]["duration"]*useDuration
+                    )
+                
+                except KeyError as keyErr:
+                    print(f"Error: Problem accessing data in Settings.json, returning default microphone handler\nErrorINFO: {keyErr}")
+                    return MicrophoneHandler()
+            
+        except FileNotFoundError as fileErr:
+            print("Error: Could not find Settings.json, make sure it is in the relative Data directory. Skipping the settings load sequence, this may lead to undefined program behaviour")
+            return MicrophoneHandler()
+    
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from MicrophoneHandler import *
 
-micHandler = MicHandler()
-micHandler.GetPhrase()
-micHandler.SaveMicrophone()
+# Add NLTK for similarity matching for given actions defined in Commands.json
+# Implement settings.json for actually deciding logic at runtime
+
+
+
+def main():
+
+    running = True
+
+    if __name__ == "__main__":
+
+        micHandler = MicrophoneHandler.initMicrophone()
+        micHandler.SaveMicrophone()
+
+        while running:
+            micHandler.GetPhrase()
+
+
+main()
